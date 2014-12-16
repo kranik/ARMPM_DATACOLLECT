167cddf8885821d9138512c7b20eb352f4a9193f
Kris Nikov <kris.nikov@bristol.ac.uk>
2014-12-16 21:40:59 +1030

    Moved event collection out of benchmarks. Modified the usage script. Added the apatch to the commit

--- CollectModel/ODROID_XU3/get_cpu_usage.sh
+++ CollectModel/ODROID_XU3/get_cpu_usage.sh
@@ -4,37 +4,113 @@
 #      user  nice system   idle iowait irq softirq steal guest guest_nice
 # cpu0 30404 2382   6277 554768   6061   0      19    0      0          0
 
-if (( $# != 1 )); then
-  echo "This program requires a wait input for the sensors in nsecs." >&2
+if [ "$#" -eq 0 ]; then
+  echo "This program requires inputs. Type -h for help." >&2
   exit 1
 fi
 
-echo -e "#Timestamp\t\tCPU_ID\tuser%\tnice%\tsys%\tidle%\tiowait%\tirq%\tsoftiq%\tsteal%\tguest%\tguest_nice%"
+#Flags to enable different functionality
+SAMPLE_TIME=0
+CPU=-1
+
+#main loop b=big L=LITTLE s=save directory n=specify number of runs -t=benchmark directory -h=help
+#requires getops, but this should not be an issue since ints built in bash
+while getopts ":bLht:" opt;
+do
+    case $opt in
+        b|L)
+            #Set flag name
+                if [[ $opt == L && $CPU == -1 ]]; then
+                        CPU=0
+                elif [[ $opt == b && $CPU == -1 ]]; then
+                        CPU=1
+                else
+                        echo "Invalid input: option -$opt has already been used!" >&2
+                        exit 1
+                fi
+                ;;
+
+        #specify the benchmark executable to be ran
+        t)
+            if (( $SAMPLE_TIME )); then
+                echo "Invalid input: option -t has already been used!" >&2
+                exit 1
+            fi
+            if (( !$OPTARG )); then
+                echo "Invalid input: option -n needs to have a positive integer!" >&2
+                exit 1
+            else
+		SAMPLE_TIME=$OPTARG
+            fi
+            ;;
+
+        h)
+            echo "Available flags and options:" >&2
+            echo "-b -> turn on collection for big core (CPU4)"
+            echo "-L -> turn on collection for LITTLE core (CPU0)"
+            echo "-t [NUMBER] -> specify sample frequency for event collection in ns."
+            echo "Mandatory options are: -b or -L; -t [NUM]"
+            echo "You can group flags with no options together, flags are separated with spaces"
+            exit 0
+            ;;
+
+        :)
+            echo "Option: -$OPTARG requires an argument" >&2
+            exit 1
+            ;;
+
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+
+    esac
+done
+
+#Check if user has specified something to run
+if (( $CPU == -1 )); then
+        echo "Please select CPU affinity with -b or -L flags" >&2
+        exit 1
+fi
+
+if (( !$SAMPLE_TIME )); then
+        echo "Invalid input: option -s (sample time) has not been specified!" >&2
+        exit 1
+fi
+
+echo -e "#Timestamp\tCPU_ID\tuser%\tnice%\tsys%\tidle%\tiowait%\tirq%\tsoftiq%\tsteal%\tguest%\tguest_nice%"
 
 declare -a CPUs
 declare -a CPUs_st
 declare -a CPUs_nd
 declare -a CPUs_idle
+
+
 time_convert=1000000000
 
 CPUs=($(awk '{if($1 == "processor") print $3}' /proc/cpuinfo))
 
 A=($(sed -n '2,9p' /proc/stat))
-last_cpu=$( echo "scale = 0;  ${#CPUs[@]} - 1;" | bc)
 
-for i in `seq 0 $last_cpu`
+if (( $CPU == -1 )); then
+	CPU=0
+	last_cpu=$( echo "scale = 0;  ${#CPUs[@]} - 1;" | bc)
+else
+	last_cpu=$CPU
+fi
+
+for i in `seq $CPU $last_cpu`
 do
 	CPUs_st[$i]=$(echo "scale = 2;  ${A[$i*11+1]}  + ${A[$i*11+2]}  + ${A[$i*11+3]}  + ${A[$i*11+4]}  + ${A[$i*11+5]}  + ${A[$i*11+6]}  + ${A[$i*11+7]}  + ${A[$i*11+8]}  + ${A[$i*11+9]}  + ${A[$i*11+10]};" | bc)
-	
 done
 
 while true; do
 	#This is to convert the user input of usecs to secs, required ofr the sleep call
-	sleep `echo "scale = 10; $1/$time_convert;" | bc`
+	sleep `echo "scale = 10; $SAMPLE_TIME/$time_convert;" | bc`
 	C=($(sed -n '2,9p' /proc/stat))
 	timestamp=$(date +'%s%N')
 
-	for i in `seq 0 $last_cpu`
+	for i in `seq $CPU $last_cpu`
 	do
         	CPUs_nd[$i]=$(echo "scale = 2;  ${C[$i*11+1]}  + ${C[$i*11+2]}  + ${C[$i*11+3]}  + ${C[$i*11+4]}  + ${C[$i*11+5]}  + ${C[$i*11+6]}  + ${C[$i*11+7]}  + ${C[$i*11+8]}  + ${C[$i*11+9]}  + ${C[$i*11+10]};" | bc)
 
@@ -58,7 +134,5 @@ while true; do
 :
 		CPUs_st[$i]=${CPUs_nd[$i]}	
 	done
-
-	echo -e "\n"	
 done
 
