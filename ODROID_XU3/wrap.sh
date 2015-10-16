b6e9ac4f93b5fc1815f34b7f84eb401bd6a52cf9
Kris Nikov <kris.nikov@bristol.ac.uk>
2015-10-16 07:23:17 +0100

    Latest set of modifications to datacollect and some preliminary results from event collection.

--- ODROID_XU3/wrap.sh
+++ ODROID_XU3/wrap.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-./MC_XU3.sh -b 1 -f 2000000,1900000,1800000,1700000,1600000,1500000,1400000,1300000,1200000,1100000,1000000,900000,800000,700000,600000,500000,400000,300000,200000 -n 3 -x /home/odroid/Work/ARMPM/ARMPM_datacollect/Workloads/cBench/O3/all_run__1_dataset_timestamp -e /home/odroid/Work/ARMPM/ARMPM_datacollect/ODROID_XU3/events_list_1.data -t 500000000 -s /home/odroid/Work/ARMPM/Results/eventsTest_list1/big/
+./MC_XU3.sh -b 1 -f 2000000,1900000,1800000,1700000,1600000,1500000,1400000,1300000,1200000,1100000,1000000,900000,800000,700000,600000,500000,400000,300000,200000 -n 3 -x /home/odroid/Work/ARMPM/ARMPM_datacollect/Workloads/cBench/O3/all_run__1_dataset_timestamp -e /home/odroid/Work/ARMPM/ARMPM_datacollect/ODROID_XU3/events_list_1.data -t 500000000 -s /home/odroid/Work/ARMPM/Results/XU3_2/eventsTest_minimal/eventsTest_list1/big/
 
-sleep 1h
+sleep 15m
 
-./MC_XU3.sh -L 1 -f 1400000,1300000,1200000,1100000,1000000,900000,800000,700000,600000,500000,400000,300000,200000 -n 3 -x /home/odroid/Work/ARMPM/ARMPM_datacollect/Workloads/cBench/O3/all_run__1_dataset_timestamp -e /home/odroid/Work/ARMPM/ARMPM_datacollect/ODROID_XU3/events_list_1.data -t 500000000 -s /home/odroid/Work/ARMPM/Results/eventsTest_list1/LITTLE/
+./MC_XU3.sh -L 1 -f 1400000,1300000,1200000,1100000,1000000,900000,800000,700000,600000,500000,400000,300000,200000 -n 3 -x /home/odroid/Work/ARMPM/ARMPM_datacollect/Workloads/cBench/O3/all_run__1_dataset_timestamp -e /home/odroid/Work/ARMPM/ARMPM_datacollect/ODROID_XU3/events_list_1.data -t 500000000 -s /home/odroid/Work/ARMPM/Results/XU3_2/eventsTest_minimal/eventsTest_list1/LITTLE/
 
-sleep 1h
+sleep 15m
 
-./MC_XU3.sh -b 1 -f 2000000,1900000,1800000,1700000,1600000,1500000,1400000,1300000,1200000,1100000,1000000,900000,800000,700000,600000,500000,400000,300000,200000 -n 3 -x /home/odroid/Work/ARMPM/ARMPM_datacollect/Workloads/cBench/O3/all_run__1_dataset_timestamp -e /home/odroid/Work/ARMPM/ARMPM_datacollect/ODROID_XU3/events_list_2.data -t 500000000 -s /home/odroid/Work/ARMPM/Results/eventsTest_list2/big/
+./MC_XU3.sh -b 1 -f 2000000,1900000,1800000,1700000,1600000,1500000,1400000,1300000,1200000,1100000,1000000,900000,800000,700000,600000,500000,400000,300000,200000 -n 3 -x /home/odroid/Work/ARMPM/ARMPM_datacollect/Workloads/cBench/O3/all_run__1_dataset_timestamp -e /home/odroid/Work/ARMPM/ARMPM_datacollect/ODROID_XU3/events_list_2.data -t 500000000 -s /home/odroid/Work/ARMPM/Results/XU3_2/eventsTest_minimal/eventsTest_list2/big/
 
-sleep 1h
+sleep 15m
 
-./MC_XU3.sh -L 1 -f 1400000,1300000,1200000,1100000,1000000,900000,800000,700000,600000,500000,400000,300000,200000 -n 3 -x /home/odroid/Work/ARMPM/ARMPM_datacollect/Workloads/cBench/O3/all_run__1_dataset_timestamp -e /home/odroid/Work/ARMPM/ARMPM_datacollect/ODROID_XU3/events_list_2.data -t 500000000 -s /home/odroid/Work/ARMPM/Results/eventsTest_list2/LITTLE/
+./MC_XU3.sh -L 1 -f 1400000,1300000,1200000,1100000,1000000,900000,800000,700000,600000,500000,400000,300000,200000 -n 3 -x /home/odroid/Work/ARMPM/ARMPM_datacollect/Workloads/cBench/O3/all_run__1_dataset_timestamp -e /home/odroid/Work/ARMPM/ARMPM_datacollect/ODROID_XU3/events_list_2.data -t 500000000 -s /home/odroid/Work/ARMPM/Results/XU3_2/eventsTest_minimal/eventsTest_list2/LITTLE/
 
+#./MC_XU3.sh -b 1 -f 2000000,1900000,1800000,1700000,1600000,1500000,1400000,1300000,1200000,1100000,1000000,900000,800000,700000,600000,500000,400000,300000,200000 -n 3 -x /home/odroid/Work/ARMPM/ARMPM_datacollect/Workloads/cBench/O3/all_run__1_dataset_timestamp -e /home/odroid/Work/ARMPM/ARMPM_datacollect/ODROID_XU3/events_list_PPS.data -t 500000000 -s /home/odroid/Work/ARMPM/Results/PPS_XU3_2/big/
 
-#./MC_XU3.sh -b 2000000,1900000,1800000,1700000,1600000,1500000,1400000,1300000,1200000,1100000,1000000,900000,800000,700000,600000,500000,400000,300000,200000 -L 1400000,1300000,1200000,1100000,1000000,900000,800000,700000,600000,500000,400000,300000,200000 -s /home/odroid/Work/ARM_PowerModel/Results -x /home/odroid/Work/ARM_PowerModel/Benchmarks/cBench/O3/all_run__1_dataset_timestamp_noevents.sh -t 500000000 -n 3
+#sleep 1h
+
+#./MC_XU3.sh -L 1 -f 1400000,1300000,1200000,1100000,1000000,900000,800000,700000,600000,500000,400000,300000,200000 -n 3 -x /home/odroid/Work/ARMPM/ARMPM_datacollect/Workloads/cBench/O3/all_run__1_dataset_timestamp -e /home/odroid/Work/ARMPM/ARMPM_datacollect/ODROID_XU3/events_list_PPS.data -t 500000000 -s /home/odroid/Work/ARMPM/Results/PPS_XU3_2/LITTLE/
 
 #./MC_XU3.sh -L 1400000,200000 -s /home/odroid/Work/ARM_PowerModel/Results -x /home/odroid/Work/ARM_PowerModel/Benchmarks/cBench/O3/all_run__1_dataset_timestamp_noevents.sh -t 500000000 -n 2
 
