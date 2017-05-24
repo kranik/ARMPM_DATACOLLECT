96f03db00f475acad577fcff899f628c6d594205
Kris Nikov <kris.nikov@bristol.ac.uk>
2017-05-24 16:01:06 +0000

    Added Samuel experiments wrap file.

---
+++ ARMPM_datacollect/ODROID_XU3/wrap_noevents_PARSEC.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#1Core
+
+#Overhead
+
+./MC_XU3.sh -L 1 -f 1400000 -n 10 -x /home/Work/DATACOLLECT/ARMPM_datacollect/Workloads/parsec-3.0/parsec_benchlist_timestamp_cset.sh -t 500000000 -s /home/Work/ARMPM/Results/XU3_1/cset_perfcpu_PARSEC_green_eMMC_c1_maxfreq_LITTLE/noevents/
+
+#4Core
+
+#Overhead
+
+./MC_XU3.sh -L 4 -f 1400000 -n 10 -x /home/Work/DATACOLLECT/ARMPM_datacollect/Workloads/parsec-3.0/parsec_benchlist_timestamp_cset.sh -t 500000000 -s /home/Work/ARMPM/Results/XU3_1/cset_perfcpu_PARSEC_green_eMMC_c4_maxfreq_LITTLE/noevents/
