6a6a64ebe797ba854caf20cf946cdd8194490377
Kris Nikov <kris.nikov@bristol.ac.uk>
2017-05-24 16:03:49 +0000

    Updates on save file path for Samuel wrapper.

--- ARMPM_datacollect/ODROID_XU3/wrap_noevents_PARSEC.sh
+++ ARMPM_datacollect/ODROID_XU3/wrap_noevents_PARSEC.sh
@@ -4,10 +4,10 @@
 
 #Overhead
 
-./MC_XU3.sh -L 1 -f 1400000 -n 10 -x /home/Work/DATACOLLECT/ARMPM_datacollect/Workloads/parsec-3.0/parsec_benchlist_timestamp_cset.sh -t 500000000 -s /home/Work/ARMPM/Results/XU3_1/cset_perfcpu_PARSEC_green_eMMC_c1_maxfreq_LITTLE/noevents/
+./MC_XU3.sh -L 1 -f 1400000 -n 10 -x /home/Work/DATACOLLECT/ARMPM_datacollect/Workloads/parsec-3.0/parsec_benchlist_timestamp_cset.sh -t 500000000 -s /home/Work/DATACOLLECT/Results/XU3_1/cset_perfcpu_PARSEC_green_mSD_c1_maxfreq_LITTLE/noevents/
 
 #4Core
 
 #Overhead
 
-./MC_XU3.sh -L 4 -f 1400000 -n 10 -x /home/Work/DATACOLLECT/ARMPM_datacollect/Workloads/parsec-3.0/parsec_benchlist_timestamp_cset.sh -t 500000000 -s /home/Work/ARMPM/Results/XU3_1/cset_perfcpu_PARSEC_green_eMMC_c4_maxfreq_LITTLE/noevents/
+./MC_XU3.sh -L 4 -f 1400000 -n 10 -x /home/Work/DATACOLLECT/ARMPM_datacollect/Workloads/parsec-3.0/parsec_benchlist_timestamp_cset.sh -t 500000000 -s /home/Work/DATACOLLECT/Results/XU3_1/cset_perfcpu_PARSEC_green_mSD_c4_maxfreq_LITTLE/noevents/
