09029f7be5b9b7bc0ae1f4927dbf6f2fe86d2030
Kris Nikov <kris.nikov@bristol.ac.uk>
2017-06-15 10:50:07 +0000

    Updating wrap script for c8 collection on eMMC.

--- ARMPM_datacollect/ODROID_XU3/wrap_noevents_PARSEC_SAMUEL.sh
+++ ARMPM_datacollect/ODROID_XU3/wrap_noevents_PARSEC_SAMUEL.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #1Core LITTLE
-./MC_XU3.sh -L 1 -f 1400000 -n 2 -x /home/Work/DATACOLLECT/ARMPM_datacollect/Workloads/parsec-3.0/parsec_benchlist_timestamp_cset.sh -t 500000000 -s /home/Work/DATACOLLECT/Results/XU3_1/cset_perfcpu_PARSEC_green_eMMC_c1_LITTLE_maxfreq/noevents/
+#./MC_XU3.sh -L 1 -f 1400000 -n 2 -x /home/Work/DATACOLLECT/ARMPM_datacollect/Workloads/parsec-3.0/parsec_benchlist_timestamp_cset.sh -t 500000000 -s /home/Work/DATACOLLECT/Results/XU3_1/cset_perfcpu_PARSEC_green_eMMC_c1_LITTLE_maxfreq/noevents/
 
 #4Core LITTLE
-./MC_XU3.sh -L 4 -f 1400000 -n 2 -x /home/Work/DATACOLLECT/ARMPM_datacollect/Workloads/parsec-3.0/parsec_benchlist_timestamp_cset.sh -t 500000000 -s /home/Work/DATACOLLECT/Results/XU3_1/cset_perfcpu_PARSEC_green_eMMC_c4_LITTLE_maxfreq/noevents/
+#./MC_XU3.sh -L 4 -f 1400000 -n 2 -x /home/Work/DATACOLLECT/ARMPM_datacollect/Workloads/parsec-3.0/parsec_benchlist_timestamp_cset.sh -t 500000000 -s /home/Work/DATACOLLECT/Results/XU3_1/cset_perfcpu_PARSEC_green_eMMC_c4_LITTLE_maxfreq/noevents/
 
-#Runs
-#./MC_XU3.sh -b 4 -L 4 -f 1000000,900000,800000,700000,600000,500000,400000,300000,200000 -q 700000,600000,500000,400000 -n 10 -x /home/Work/DATACOLLECT/ARMPM_datacollect/Workloads/parsec-3.0/parsec_benchlist_timestamp_cset.sh -t 500000000 -s /home/Work/DATACOLLECT/Results/XU3_1/cset_perfcpu_PARSEC_green_mSD_c8_allcores/noevents/
+#All Cores
+./MC_XU3.sh -b 4 -L 4 -f 1000000,900000,800000,700000,600000,500000,400000,300000,200000 -q 1400000,1300000,1200000,1100000,1000000,900000,800000,700000,600000,500000,400000,300000,200000 -n 2 -x /home/Work/DATACOLLECT/ARMPM_datacollect/Workloads/parsec-3.0/parsec_benchlist_timestamp_cset.sh -t 500000000 -s /home/Work/DATACOLLECT/Results/XU3_1/cset_perfcpu_PARSEC_green_eMMC_c8/noevents/
 
